3cf359c9f8e451016031ee2fb79249d68680e3ac
VenchasS <49115035+VenchasS@users.noreply.github.com>
2024-03-01 17:42:05 +0500

    delete copy

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'prettier',
     'plugin:react/recommended',
